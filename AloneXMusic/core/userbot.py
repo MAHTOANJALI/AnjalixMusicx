5dff4c741153c49d9dc930a2ff440a403341c8b4
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-28 00:05:09 +0530

    Update userbot.py

--- AloneXMusic/core/userbot.py
+++ AloneXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxChannel")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.two.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxChannel")
             except:
                 pass
             assistants.append(2)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.four.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxChannel")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.five.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxChannel")
             except:
                 pass
             assistants.append(5)
