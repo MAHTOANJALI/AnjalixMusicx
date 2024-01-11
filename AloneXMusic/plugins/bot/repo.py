882195e0076ef0807c8c462a036fe77dcd6370cf
MAHTO ANJALI <154720925+MAHTOANJALI@users.noreply.github.com>
2024-01-11 20:52:41 +0530

    Update repo.py

--- AloneXMusic/plugins/bot/repo.py
+++ AloneXMusic/plugins/bot/repo.py
@@ -48,7 +48,7 @@ async def start(_, msg):
 #[
 #InlineKeyboardButton("𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/TeamAloneOp/AloneXChatBot"),
 #InlineKeyboardButton("𝗜𝗗 𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/TeamAloneOp/AloneXIdChatbot"),
-],
+#],
 [
             #  InlineKeyboardButton("𝗦𝗣𝗔𝗠", url=f"https://github.com/TeamAloneOp/AloneXSpam"),
             #  InlineKeyboardButton("𝗕𝗔𝗡𝗔𝗟𝗟", url=f"https://github.com/TeamAloneOp/AloneXBanallBot"),
